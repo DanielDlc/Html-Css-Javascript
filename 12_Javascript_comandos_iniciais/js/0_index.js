d3914ca4a1dbabf78ab900026a2bffff0f2ea43d
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-26 11:15:23 -0300

    ola mundo js css e html

---
+++ 12_Javascript_comandos_iniciais/js/0_index.js
@@ -0,0 +1,9 @@
+function gritar() {
+  alert("Ola, Mundo JAVASCRIPT!");
+}
+
+var nome = prompt("Digite seu nome: ")
+var idade = prompt("Qual sua idade: ");
+
+document.write("Bem vindo(a), ", nome);
+alert("Ola! " + nome +" você tem apenas " + idade + " anos de idade");
