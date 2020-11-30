dd163ada72ec6eecc895ae1f11261d81ae9910ab
DanielDlc <dlc.engcomputacao@gmail.com>
2020-11-30 18:57:11 -0300

    Avaliar verdadeiro ou falso

---
+++ 12_Javascript_comandos_iniciais/js/08_curto_circuito.js
@@ -0,0 +1,22 @@
+/* 
+&& -> false && true -> false " o valor é o mesmo"
+|| ->
+false  literal
+valores avaliádos em falsy (false, 0, ' ', " ", ` `, null, undefined, NaN)
+*/
+
+// && true significa que todas expressões são veradeira e me retorna o último verdadeiro.
+console.log('Daniel Costa' && true && 'Bianca');
+
+// || -> or precisa de apenas um para retornar verdadeiro.
+console.log(0 || false || null || 'Daniel Costa' || True);  // retornou o primeiro valor verdadeiro.
+
+
+
+
+
+
+
+
+
+
