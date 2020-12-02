3b1f5553721138da55b793bc8a2e618838917501
DanielDlc <dlc.engcomputacao@gmail.com>
2020-12-01 22:40:40 -0300

    incremento e decremento no laço for

---
+++ 14_javascript_laço_for/01_repetit_com_for.js
@@ -0,0 +1,13 @@
+// Criar uma variável de controle
+/* variável i inicia em 0 e a variável i é menor ou igual a 50,
+   variavel i inicia em 1 após o incremento e fica no laço até chegar na linha 50 
+   sempre pulando de 1 em 1 pois i++*/
+for (let i = 0; i <= 50; i++) {
+    console.log(`Linha ${i}`);
+
+
+console.log("");  // pular uma linha
+
+for (let i = 10; i >= 0; i--) {  // contagem regressiva com decremento.
+    console.log(`Contagem regressiva ${i}`);
+}
